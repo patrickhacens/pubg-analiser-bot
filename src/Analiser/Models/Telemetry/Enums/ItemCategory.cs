9050d0ab6f4ed08ff220aad1a90bc50726c96e68
Patrick Ens <patrick.ens@sp.senai.br>
2022-06-04 15:18:32 -0300

    belt subcategory

--- src/Analiser/Models/Telemetry/Enums/ItemCategory.cs
+++ src/Analiser/Models/Telemetry/Enums/ItemCategory.cs
@@ -7,6 +7,7 @@
         Equipment,
         Event,
         Use,
-        Weapon
+        Weapon,
+        Belt
     }
 }
--- src/Analiser/Models/Telemetry/Enums/ItemSubCategory.cs
+++ src/Analiser/Models/Telemetry/Enums/ItemSubCategory.cs
@@ -17,6 +17,7 @@
         Revive,
         Sight,
         Throwable,
-        Vest
+        Vest,
+        Belt
     }
 }
